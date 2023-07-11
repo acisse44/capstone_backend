e2ca3c2fd569050050f4e19f1b2659499f9c9312
rana <abedorana7@gmail.com>
2023-07-11 16:46:26 -0400

    add routes for achievement, language, lessons in  api folder

--- api/users.js
+++ api/users.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const router = express.Router();
-// const {  } = require("../db/models");
+const { User } = require("../db/models");
 
-router.get("/", async (request, response, next) => {
+router.get("/allusers", async (request, response, next) => {
     try {
-        
+        const allUsers = await User.findAll({attributes: ["userID", "username"]}); 
+        response.status(200).json(allUsers);
     } catch (error) {
-        
-    }
+        next(error);
+    };
 });
 
+// router.post("/achievements", async (request, response, next) => {
+//     try {
+//         const 
+//     } catch (error) {
+//         next(error);
+//     }
+// });
 
 module.exports = router;
