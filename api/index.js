d80d264ac7947577c926a152e268932230d27db3
rana <abedorana7@gmail.com>
2023-07-11 15:35:12 -0400

    add first 5 tables to api folder & add index.js

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const router = require("express").Router;
+
+//mount on api:
+//router.use("/filename, require("./filename"));
+
+router.use("/users", require("./users"));
+router.use("/languages", require("./languages"));
+router.use("/lessons", require("./lessons"));
+router.use("/friends", require("./friends"));
+router.use("/achievements", require("./achievements"));
+
+
+router.use((req, res, next) => {
+    const error = new Error("404 Not Found");
+    error.status = 404;
+    next(error);
+});
+
+module.exports = router;
---
+++ api/users.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+// const {  } = require("../db/models");
+
+router.get("/", async (request, response, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+
+module.exports = router;
