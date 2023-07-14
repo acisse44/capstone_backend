b4d67e35679f2a9bfe84659738e4e46d169ee88f
rana <abedorana7@gmail.com>
2023-07-14 15:27:46 -0400

    [feature] add chat connected to page

--- server.js
+++ server.js
@@ -16,10 +16,28 @@ const sessionStore = new SequelizeStore({ db });
 
 
 //socket io setup
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('chat message', (msg) => {
+//     io.emit('chat message', msg);
+//     console.log('message: ' + msg);
+//   });
+  
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+    console.log(msg);
   });
 });
 
