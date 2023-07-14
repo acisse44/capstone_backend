026d15fd1be4065e6e97c1ca76d99cf8807ef31c
Aminata <aminata.cisse44@bcmail.cuny.edu>
2023-07-14 08:41:44 -0400

    force true

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ const setupRoutes = (app) => {
 
 // Start server and sync the db
 const startServer = async (app, port) => {
-  await db.sync();
+  await db.sync({force: true});
   app.listen(port, () => console.log(`Server is on port:${port}`));
   return app;
 };
