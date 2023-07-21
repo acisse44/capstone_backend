984b2ad4d584da93472e7119edffa5e63408eb91
Aminata <aminata.cisse44@bcmail.cuny.edu>
2023-07-21 09:32:31 -0400

    [feature] language questions updated

--- db/models/avatar.js
+++ db/models/avatar.js
@@ -3,7 +3,7 @@ const db = require("../db.js")
 //const User = require("./user.js"); // Import the User model
 
 
-//Our achievement table with the following attributes for each
+//Our avatar table with the following attributes for each
 const Avatar = db.define("avatar", {
   imageURL: {
     type: DataTypes.TEXT,
