0c5d21202404374df3255e3df4846935c1f1f3d3
JiaminShi1226 <shijiamin0324@gmail.com>
2023-07-27 10:25:20 -0400

    Fixed Error

--- db/models/UserAchievement.js
+++ db/models/UserAchievement.js
@@ -1,21 +1,21 @@
-const { DataTypes } = require("sequelize");
-const db = require("../db.js");
+// const { DataTypes } = require("sequelize");
+// const db = require("../db.js");
 
-const UserAchievement = db.define("userAchievement", {
-  userId: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "Users",
-      key: "id",
-    },
-  },
-  achievementId: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "achievements",
-      key: "id",
-    },
-  },
-});
+// const UserAchievement = db.define("userAchievement", {
+//   userId: {
+//     type: DataTypes.INTEGER,
+//     references: {
+//       model: "Users",
+//       key: "id",
+//     },
+//   },
+//   achievementId: {
+//     type: DataTypes.INTEGER,
+//     references: {
+//       model: "achievements",
+//       key: "id",
+//     },
+//   },
+// });
 
-module.exports = UserAchievement;
+// module.exports = UserAchievement;
